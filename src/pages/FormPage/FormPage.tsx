ae4e6e087519c798e20fe0282eb1fe501d980b97
Danila Emelchenko <emelchenko1997@yandex.ru>
2025-03-18 17:58:23 +0300

    Added a page with answers to questions and a timer

--- src/pages/FormPage/FormPage.tsx
+++ src/pages/FormPage/FormPage.tsx
@@ -1,3 +1,69 @@
+import {
+  Box,
+  Button,
+  Container,
+  Paper,
+  Radio,
+  RadioGroup,
+  FormControlLabel,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+
 export const FormPage = () => {
-  return <div>FormPage</div>;
+  const [selectedAnswer, setSelectedAnswer] = useState("");
+
+  return (
+    <Container maxWidth="sm">
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        height="100vh"
+      >
+        {/* Таймер */}
+        <Typography variant="h6" color="error">
+          Оставшееся время: 00:30
+        </Typography>
+
+        {/* Вопрос */}
+        <Paper elevation={3} sx={{ padding: 4, mt: 3, width: "100%" }}>
+          <Typography variant="h5" gutterBottom>
+            Вопрос 1: Какой цвет небо?
+          </Typography>
+
+          {/* Варианты ответов */}
+          <RadioGroup
+            value={selectedAnswer}
+            onChange={(e) => setSelectedAnswer(e.target.value)}
+          >
+            <FormControlLabel value="red" control={<Radio />} label="Красный" />
+            <FormControlLabel
+              value="yellow"
+              control={<Radio />}
+              label="Жёлтый"
+            />
+            <FormControlLabel value="blue" control={<Radio />} label="Синий" />
+            <FormControlLabel
+              value="green"
+              control={<Radio />}
+              label="Зелёный"
+            />
+          </RadioGroup>
+        </Paper>
+
+        {/* Кнопка "Следующий вопрос" */}
+        <Button
+          variant="contained"
+          color="primary"
+          sx={{ mt: 3 }}
+          fullWidth
+          disabled={!selectedAnswer} // Отключено, пока не выбран ответ
+        >
+          Следующий вопрос
+        </Button>
+      </Box>
+    </Container>
+  );
 };
